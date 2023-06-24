92bfe4be410e979972f6b8323e13cb54ef75edf8
Renz 8:28 <110266073+RenzKing@users.noreply.github.com>
2023-06-25 00:20:12 +0800

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -121,7 +121,12 @@ function Footer() {
   console.log(isOpen);
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
